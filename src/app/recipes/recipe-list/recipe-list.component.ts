f595bbc6513f0b78b51eeaccf5577ae11e7b01fd
George Eaton <george_eaton@hotmail.co.uk>
2017-03-31 12:37:35 +0100

    recipe list iterates over recipes array to display each recipe on page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Just a test', 'http://www.japanesecooking101.com/wp-content/uploads/2014/01/IMG_7897.jpg'),
+    new Recipe('Another Test Recipe', 'It\'s delicious!', 'https://i.ytimg.com/vi/GWxACwK8KeA/maxresdefault.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit() {
